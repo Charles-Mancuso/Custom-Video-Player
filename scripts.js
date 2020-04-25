026693c708c83ec2498f556f6e234f624a65c154
charles mancuso <charlesmancuso66@yahoo.com>
2020-04-25 11:54:10 -0700

    Add update button function

--- scripts.js
+++ scripts.js
@@ -5,3 +5,22 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚❚';
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', togglePlay);
+video.addEventListener('pause', togglePlay);
+
+toggle.addEventListener('click', togglePlay);
